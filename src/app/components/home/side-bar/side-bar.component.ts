a13636725935981885793ed2f4bd5090377a223c
Agamy <mohamedagamy327@gmail.com>
2020-10-28 15:57:51 +0200

    rtl

--- src/app/components/home/side-bar/side-bar.component.ts
+++ src/app/components/home/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -11,14 +12,31 @@ export class SideBarComponent {
 
   currentLang = 'en';
 
+  @Inject(DOCUMENT) private document: Document
+
   constructor(
     public translate: TranslateService
   ) {
   }
-
   setLang(lang: any) {
+    if (lang === 'ar') {
+      this.setDynamicStyle('./assets/css/Style-ar.css');
+      this.setDynamicStyle('./assets/plugins/bootstrap-rtl/css/bootstrap-rtl.min.css');
+    }
+    else {
+      window.location.reload();
+    }
     this.currentLang = lang;
     this.translate.use(lang);
+  }
+
+  setDynamicStyle(cssURL: string) {
+    const head = document.getElementsByTagName('head')[0];
+    const style = document.createElement('link');
+    style.id = 'css-styling';
+    style.rel = 'stylesheet';
+    style.href = `${cssURL}`;
+    head.appendChild(style);
   }
 
 }
