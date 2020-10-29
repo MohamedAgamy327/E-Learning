796700a96891cf99d595e8b22a53569251e20259
Agamy <mohamedagamy327@gmail.com>
2020-10-29 16:38:20 +0200

    login

--- src/app/components/register/login/login.component.ts
+++ src/app/components/register/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
@@ -9,19 +11,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  currentLang = 'en';
 
-
+  @Inject(DOCUMENT) private document: Document
 
   constructor(
+    public translate: TranslateService
   ) {
-
+  }
+  setLang(lang: any) {
+    if (lang === 'ar') {
+      this.setDynamicStyle('./assets/css/Style-ar.css');
+      this.setDynamicStyle('./assets/plugins/bootstrap-rtl/css/bootstrap-rtl.min.css');
+    }
+    else {
+      window.location.reload();
+    }
+    this.currentLang = lang;
+    this.translate.use(lang);
   }
 
-
-
-  ngOnInit() {
-
+  setDynamicStyle(cssURL: string) {
+    const head = document.getElementsByTagName('head')[0];
+    const style = document.createElement('link');
+    style.id = 'css-styling';
+    style.rel = 'stylesheet';
+    style.href = `${cssURL}`;
+    head.appendChild(style);
   }
 
 
